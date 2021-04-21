bed49212badca1a785eaafdf644a1cea2a198739
zangassis <assiszang@gmail.com>
2021-04-21 20:18:13 -0300

    fix: Alter productController

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,41 +1,42 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using stock_manager.Models;
 
 namespace stock_manager.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
     public class ProductsController : Controller
     {
         private readonly Context _context;
-    }
+        
 
-    public ProductsController(Context context)
-    {
-        _context = context;
-    }
-
-    public async Task(ActionResult) Index()
-    {
-        return View(await _context.Products.Incude(x => x.Categtory).ToListAssynk();    
-    }
-
-    [HttpGet]
-    public async Task<ActionResult> NewProduct()
-    {
-        viewData["Category"] = new SelectList(await _context.Categories.ToListAsync(), "CategoryId", "Name")
-    }
-
-    [HttpPost]
-    public async Task<ActionResult> NewProduct(Product product) 
-    {
-        if(ModelState.IsValid)
+        public ProductsController(Context context)
         {
-            await _context.Products.AddAsync();
-            await _context.SaveChangesAsync();
-            return RedirectToAction(named(Index));
+            _context = context;
         }
 
-        return View(product);
-    }
+        public async Task<ActionResult> Index()
+        {
+            return View(await _context.Products.Include(x => x.Categtory).ToListAssynk());    
+        }
 
+        [HttpGet]
+        public async Task<ActionResult> NewProduct()
+        {
+            ViewData["CategoryId"] = new SelectList(await _context.Categories.ToListAsync(), "CategoryId", "Name");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewProduct(Product product) 
+        {
+            if(ModelState.IsValid)
+            {
+                await _context.Products.AddAsync();
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(await _context.Categories.ToListAsync(), "CategoryId", "Name", product.CategoryId);
+            return View(product);
+        }
+    }
 }              
